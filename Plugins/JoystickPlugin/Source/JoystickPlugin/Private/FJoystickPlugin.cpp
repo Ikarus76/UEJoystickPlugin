699f87e8fa8b32344156ccf7caa1b793a1f4dcec
Sam Persson <samiljan@gmail.com>
2015-01-19 19:46:04 +0100

    Wrong axis value sent to event

--- Plugins/JoystickPlugin/Source/JoystickPlugin/Private/FJoystickPlugin.cpp
+++ Plugins/JoystickPlugin/Source/JoystickPlugin/Private/FJoystickPlugin.cpp
@@ -172,32 +172,6 @@ bool EmitAnalogInputEventForKey(FKey key, float value, int32 user, bool repeat)
 	return FSlateApplication::Get().ProcessAnalogInputEvent(AnalogInputEvent);
 }
 
-FVector2D POVDirectionToVector(JoystickPOVDirection povValue)
-{
-	switch (povValue){
-	case DIRECTION_NONE:
-		return FVector2D(0, 0);
-	case DIRECTION_UP:
-		return FVector2D(0, 1);
-	case DIRECTION_UP_RIGHT:
-		return FVector2D(1, 1);
-	case DIRECTION_RIGHT:
-		return FVector2D(1, 0);
-	case DIRECTION_DOWN_RIGHT:
-		return FVector2D(1, -1);
-	case DIRECTION_DOWN:
-		return FVector2D(0, -1);
-	case DIRECTION_DOWN_LEFT:
-		return FVector2D(-1, -1);
-	case DIRECTION_LEFT:
-		return FVector2D(-1, 0);
-	case DIRECTION_UP_LEFT:
-		return FVector2D(-1, 1);
-	default:
-		return FVector2D(0, 0);
-	}
-}
-
 /** Internal Tick - Called by the Plugin */
 void FJoystickPlugin::DelegateTick(float DeltaTime)
 {
@@ -253,7 +227,7 @@ void FJoystickPlugin::DelegateTick(float DeltaTime)
 		if (current.RAxis != prev.RAxis)
 		{
 			if (joystickDelegate)
-				joystickDelegate->RAxisChanged(current.Axis, current);
+				joystickDelegate->RAxisChanged(current.RAxis, current);
 			EmitAnalogInputEventForKey(EKeysJoystick::JoystickRAxisX, current.RAxis.X, p, 0);
 			EmitAnalogInputEventForKey(EKeysJoystick::JoystickRAxisY, current.RAxis.Y, p, 0);
 			EmitAnalogInputEventForKey(EKeysJoystick::JoystickRAxisZ, current.RAxis.Z, p, 0);
