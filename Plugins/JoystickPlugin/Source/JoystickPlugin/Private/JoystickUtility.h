8172c0ed8327ffd8c5f0b97a20cd146616dcbf5a
tsky <thomas.kollakowsky@gmail.com>
2015-02-17 10:21:58 +0100

    Ahrg.... failed to copy to the right place - sorry

--- Plugins/JoystickPlugin/Source/JoystickPlugin/Private/JoystickUtility.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-DECLARE_LOG_CATEGORY_EXTERN(JoystickPluginLog, Log, All);
-
-FVector JoystickUtilityNormalizeAxis(FVector in);
-FVector2D JoystickUtilityNormalizeSlider(FVector2D value);
-float JoystickUtilityNormalizeForceValue(int32 value);
-int32 JoystickUtilityDeNormalizeForceValue(float value);
